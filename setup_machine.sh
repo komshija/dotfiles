af686d854ebd18692eceb3f1baa27c16e616921e
Radosavljevic <milan.radosavljevic@zuehlke.com>
2025-03-07 15:55:51 +0100

    update

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/bash
 
+## Helpers
 setup_start()
 {
 	echo "#############"
@@ -17,16 +19,16 @@ setup_end()
 
 setup_packages()
 {
-	setup_start "Setup packages"
+	setup_start "setting up packages"
 
 	sudo apt update
 	sudo apt upgrade -y
 
-	sudo apt install -y gnome-tweaks \
+	sudo apt install -y \
 			 git \
-			 chrome-gnome-shell \
 			 g++ \
 			 clang \
+			 cmake \
 			 clang-format \
 			 clang-tidy \
 			 unzip \
@@ -37,40 +39,40 @@ setup_packages()
 			 autoconf \
 			 autopoint
 
-	setup_end "Setup packages"
+	setup_end "setting up packages"
 }
 
 ######################################################################################
 
 setup_directories() 
 {
-	setup_start "Setup directories"
+	setup_start "setting up directories"
 
 	mkdir -p ~/source/
 
-	setup_end "Setup directories"
+	setup_end "setting up directories"
 }
 
 ######################################################################################
 
 setup_git()
 {
-	setup_start "Setup git"
-	
-	git config --global user.email  "milan.r.radosavljevic@outlook.com"
-	git config --global user.name "Milan Radosavljevic" 
+	setup_start "setting up git"
 
-	setup_end "Setup git"
+	stow git
+
+	setup_end "setting up git"
 }
 
 ######################################################################################
 
 install_fonts()
 {
-mkdir -p ~/.fonts
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/0xProto.zip -P ~/.fonts
-unzip ~/.fonts/0xProto.zip
+	setup_start "setting up fonts"
+	
+	stow fonts
+	
+	setup_end "setting up fonts"
 }
 
 ######################################################################################
@@ -78,59 +80,64 @@ unzip ~/.fonts/0xProto.zip
 
 setup_gnome_dock()
 {
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 30
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM' 
-gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+	gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 30
+	gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM' 
+	gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+	gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
 }
 
 ######################################################################################
-# Kitty setup
+
 install_kitty()
 {
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-echo 'kitty.desktop' > ~/.config/xdg-terminals.list
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator ~/.local/bin/kitty 50
-sudo update-alternatives --config /x-terminal-emulator
-sudo update-alternatives --set x-terminal-emulator ~/.local/bin/kitty 
+	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+	ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+	cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+	cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+	sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+	sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+	echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+	sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator ~/.local/bin/kitty 50
+	sudo update-alternatives --config /x-terminal-emulator
+	sudo update-alternatives --set x-terminal-emulator ~/.local/bin/kitty 
 }
 
 ######################################################################################
-# Zsh setup
+
 
 setup_zsh()
 {
-sudo chsh -s $(which zsh)
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	sudo chsh -s $(which zsh)
+	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	# zsh-syntax-highlighting plugin
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# Install zsh-autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	# Install zsh-autosuggestions plugin
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Install Fuzzy finder
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+	# Install Fuzzy finder
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
-# Download theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+	# Download theme
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 
 ######################################################################################
-# Tmux setup
 
 setup_tmux_tpm()
 {
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	stow tmux
+	echo "Run tmux and press (Ctrl+A) + (Shift+I)" 
 }
 
+######################################################################################
 
-
-
-
-
+setup_packages
+setup_git
+install_fonts
+#setup_gnome_dock
+install_kitty
+setup_zsh
+setup_tmux_tpm
