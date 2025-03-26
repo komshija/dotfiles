2eaa9118956de85a2ce5c889fe401f4bddcc8aeb
Milan Radosavljević <milan.r.radosavljevic@outlook.com>
2025-03-26 14:38:17 +0100

    Add install_tmux.sh

---
+++ install_tmux.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+install_dependencies() {
+  sudo apt install libevent-dev ncurses-dev build-essential bison pkg-config
+}
+
+install_tmux() {
+  git clone https://github.com/tmux/tmux.git $SCRIPT_DIR/tmux_repo
+  cd tmux_repo
+  sh autogen.sh
+  ./configure
+  make && sudo make install
+}
+
+stow_tmux() {
+  cd "$SCRIPT_DIR"
+  stow --restow --target="$HOME" --dir="$PWD" tmux
+}
+
+main() {
+  install_dependencies
+  install_tmux
+  stow_tmux
+}
+
+main
